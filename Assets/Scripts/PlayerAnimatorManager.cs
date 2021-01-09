5b5454773108b96ebb5e6244583cd2e646a21883
Tim <perepeltim@gmail.com>
2021-01-09 23:10:00 +0300

    Player

---
+++ Assets/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.TimCorporation.Multiplayer
+{
+    public class PlayerAnimatorManager : MonoBehaviour
+    {
+        #region Private Fields
+
+        [SerializeField] private float directionDampTime = 0.25f;
+
+        #endregion
+
+        #region MonoBehaviour Callbacks
+
+        private Animator animator;
+
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+            if (!animator)
+            {
+                Debug.LogError("PlayerAnimatorManager is Missing Animator Component", this);
+            }
+        }
+
+        void Update()
+        {
+            if (!animator)
+            {
+                return;
+            }
+
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+            if (stateInfo.IsName("Base Layer.Run"))
+            {
+                if (Input.GetButtonDown("Fire2"))
+                {
+                    animator.SetTrigger("Jump");
+                }
+            }
+
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            if (v < 0)
+            {
+                v = Mathf.Abs(0);
+            }
+
+            animator.SetFloat("Speed", h * h + v * v);
+            animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+        }
+
+        #endregion
+    }
+}
